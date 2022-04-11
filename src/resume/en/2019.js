c2c70dd2c56f9c4a86cde60f12a00ed6861768f0
Sebastian Findling <i@seb.cl>
2022-04-10 21:40:29 -0400

    typo

--- src/resume/en/2019.js
+++ src/resume/en/2019.js
@@ -1,17 +1,14 @@
 export default {
     last: true,
-    items: [
-        {
-            title: 'CTO in Add Shop',
+    items: [{
+            title: 'CTO at Add Shop',
             description: 'Using my framework Elder as a base, we created a social network with integrated e-commerce marketplace, that won the incubation of 500 Startups in Mexico.',
-            images: [
-            ]
+            images: []
         },
         {
             title: 'MERA',
             description: 'Prototype of a multiplayer open-world game about pre-columbian America.',
-            images: [
-            ]
+            images: []
         },
     ],
 }
