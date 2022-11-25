b69f65151c16e1716af5f02a69fc88a3ed5c2fe4
Sebastian Findling <i@seb.cl>
2022-11-25 11:31:30 -0300

    2023

---
+++ src/resume/en/2023.js
@@ -0,0 +1,10 @@
+export default {
+    items: [{
+            title: 'Tech Lead in AFEX since 2021',
+            description: 'Some stuff',
+            images: [
+                { src: 'dreamoms1.jpg', alt: 'Course viewer' },
+            ]
+        },
+    ],
+}
