093a2c9d858db536aa913c57984038adc7f7362d
Sebastian Findling <i@seb.cl>
2022-11-25 13:50:51 -0300

    build

--- src/resume/en/1998.js
+++ src/resume/en/1998.js
@@ -2,7 +2,7 @@ export default {
     items: [
         {
             title: 'Club de Amigos de los Animales (CAA)',
-            description: 'At the age of 11, I created my first website written in Microsoft Frontpage, with pictures of neighbours\' pets.',
+            description: 'At the age of 11, I created my first website in Microsoft Frontpage, with pictures of neighbours\' pets.',
             images: [
             ]
         },
