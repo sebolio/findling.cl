fc068c16d0e3120653833dc4f9874808141a9e40
Sebastian Findling <i@seb.cl>
2023-01-10 12:54:13 -0300

    deploy

--- src/resume/en/2019.js
+++ src/resume/en/2019.js
@@ -2,7 +2,7 @@ export default {
     last: true,
     items: [{
             title: 'CTO at Add Shop',
-            description: 'Using my framework Elder as a base, we created a social network with integrated e-commerce marketplace, that won the incubation of 500 Startups in Mexico.',
+            description: 'Using my framework Elder as a base, we created a social network with integrated e-commerce marketplace, that won the incubation of 500 Startups in Mexico. **View our videogame presentation on [this video](https://www.youtube.com/watch?v=K3mJ1FdgNNo)**',
             images: [
                 { src: 'addshop4.jpg' },
                 { src: 'addshop2.jpg' },
