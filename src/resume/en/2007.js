e414b94bfe53e0c317a6e7757cfb2742ce92c16f
Seb <i@seb.cl>
2023-12-31 04:06:25 -0300

    ejercicios

--- src/resume/en/2007.js
+++ src/resume/en/2007.js
@@ -4,6 +4,12 @@ export default {
             title: 'Ejercicios.cl',
             description: 'Public math exercises platform which students could download and solve, in exchange for personal data.',
             images: [
+                { src: 'ejercicios1.jpg', alt: 'Home' },
+                { src: 'ejercicios2.jpg', alt: 'Inside news' },
+                { src: 'ejercicios3.jpg', alt: 'Exercise downloads' },
+                { src: 'ejercicios4.jpg', alt: '' },
+                { src: 'ejercicios5.jpg', alt: '' },
+                { src: 'ejercicios6.jpg', alt: '' },
             ]
         },
     ],
