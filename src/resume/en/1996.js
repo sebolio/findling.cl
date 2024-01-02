5badd56e8327bb4428899d6c245f6443438d26a5
Sebastian Findling <seb@MacBook-Pro-de-Sebastian.local>
2024-01-02 06:13:13 -0300

    first time is hard

---
+++ src/resume/en/1996.js
@@ -0,0 +1,11 @@
+export default {
+    items: [
+        {
+            title: 'My first program',
+            description: 'At the age of 9, thanks to a cousin who taught me BASIC programming language, I created my first piece of code.',
+            images: [
+                {src: 'hola.gif', alt: ''},
+            ]
+        },
+    ],
+}
