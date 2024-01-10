acb715b6bb46631c61b3fe880d525f017af718e7
Sebastian Findling <i@seb.cl>
2024-01-10 14:17:23 -0300

    added CVAI

---
+++ src/resume/en/2024.js
@@ -0,0 +1,20 @@
+export default {
+    items: [{
+        title: 'CV AI',
+        description: 'Online hiring platform that understands CVs and allows filtering them by using human langauge.',
+        images: [
+            { src: 'cvai1.jpg' },
+            { src: 'cvai2.jpg' },
+            { src: 'cvai3.jpg' },
+        ]
+    },
+        // {
+        //     title: 'Fichero.net',
+        //     description: 'Created an online patient-management software that can be used by any \
+        //     professional related to medicine.',
+        //     images: [
+        //         { src: 'fichero1.jpg' },
+        //     ]
+        // },
+    ],
+}
